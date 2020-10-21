48760106f674eb1a1c5485a6a2ab5c8d30de8269
chuanzhangjiang <chuanzhangjiang@outlook.com>
2020-10-21 11:43:15 +0800

    支持emoji表情

--- config.h
+++ config.h
@@ -6,9 +6,16 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 // static char *font = "Liberation Mono:pixelsize=22:antialias=true:autohint=true";
-static char *font = "Fira Code Nerd Font Mono:pixelsize=16:antialias=true:autohint=true";
+static char *font = "SauceCodePro Nerd Font:pixelsize=16:antialias=true:autohint=true";
 // static char *font = "FiraMono Nerd Font Mono:pixelsize=16:antialias=true:autohint=true";
 // static char *font = "Source Code Pro for Powerline:pixelsize=22:antialias=true:autohint=true";
+
+/* Spare fonts */
+static char *font2[] = {
+	"Symbola:pixelsize=16:antialias=true:autohint=true"
+	//	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true",
+};
+
 static int borderpx = 2;
 
 /*
